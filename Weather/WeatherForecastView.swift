8b3922503acdd51640ab823775e0ab745e602a53
Abdullah Aljahdali <alja7dali@gmail.com>
2023-02-05 00:50:12 +0300

    Update `API_KEY`

--- Weather/WeatherForecastView.swift
+++ Weather/WeatherForecastView.swift
@@ -71,7 +71,7 @@ struct WeatherForecastView: View {
     }
     .navigationTitle("\(cityName.capitalized) Weather")
     .onAppear {
-      let apiKey: String = "8f31ab935c42e9f805ef1fa7b40c8821"
+      let apiKey: String = "Put your API_KEY here"
       let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(apiKey)"
       let url = URL(string: urlString)!
       
